c34fb9c65559c933464bbecaacc6de933eccbbe9
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-10-03 19:39:42 -0300

    Fazendo o javascript do site

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,27 @@
-console.log("Teste");
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        console.log("e");
+
+        buttons.forEach((btn) => {
+            btn.querySelector(".color").classList.remove("selected");
+        });
+
+        const button = e.target
+
+        const id = button.getAtribute("id");
+
+        button.querySelector(".color").classList.add("selected");
+
+        image.classList.add("changing");
+        image.setAttribute("src", ` img/iphone_${id}.jpg`);
+
+        setTimeout(() => {
+            image.classList.toggle("changing");
+        }, 200);
+    });
+});
+
